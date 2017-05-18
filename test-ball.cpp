4e49aec34087a0dc05a518592e6e616fc65a64f2
gregsloap <gregfla2009@hotmail.com>
2017-05-17 23:13:29 -0300

    Add files via upload

--- test-ball.cpp
+++ test-ball.cpp
@@ -4,14 +4,14 @@
  **/
 
 #include "ball.h"
-void run(Simulation & s, double dt) {
- for (int i = 0 ; i < 100 ; ++i) { s.step(dt) ; s.display() ; }
- }
 
 int main(int argc, char** argv)
 {
   Ball ball ;
   const double dt = 1.0/30 ;
- run(ball, dt)
+  for (int i = 0 ; i < 100 ; ++i) {
+    ball.step(dt) ;
+    ball.display() ;
+  }
   return 0 ;
 }
